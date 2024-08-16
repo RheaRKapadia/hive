c120226300d9e2d8ecc9caab5fb65235852e58f3
RheaKapadia <rheakapadia@gmail.com>
2024-08-16 14:24:37 -0400

    linked wger api for new workouts
    Still need to clean data

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios');
 
 router.get('/', (req, res) => {
     res.render('10_workouts')
@@ -9,8 +10,20 @@ router.get('/workout', (req, res) => {
     res.render('11_workout')
 })
 
-router.get('/new', (req, res) => {
-    res.render('12_newworkout')
+router.get('/new', async (req, res) => {
+    // res.render('12_newworkout')
+    const apiUrl = 'https://wger.de/api/v2/exercise/';
+
+    try {
+        const response = await axios.get(apiUrl)
+        const exercises = response.data.results
+        const exerciseNames = exercises.map(exercise => exercise.name)
+
+        res.status(200).render('12_newworkout', {exerciseNames})
+    } catch (error) {
+        console.error('Error fetching exercises data:', error)
+        res.status(500).json({ error: 'Failed to fetch exercises data' });
+    }
 })
 
 router.get('/workout/log', (req, res) => {
