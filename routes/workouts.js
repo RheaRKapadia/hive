3f2e349b707cc53e6bb1c2a8d31e239228f572c8
RheaKapadia <rheakapadia@gmail.com>
2024-08-20 18:51:04 -0400

    exercise names and pagination to newWorkout
    need to further clean data for only english and add other info

--- routes/workouts.js
+++ routes/workouts.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const axios = require('axios');
 
+
 router.get('/', (req, res) => {
     res.render('10_workouts')
 })
@@ -11,15 +12,33 @@ router.get('/workout', (req, res) => {
 })
 
 router.get('/new', async (req, res) => {
-    // res.render('12_newworkout')
-    const apiUrl = 'https://wger.de/api/v2/exercise/';
+    //not all of these are in english so will have to clean it further & change capitalization of some of them
+    const apiUrl = 'https://wger.de/api/v2/exercise';
+    // /?language=2&limit=50
+    const limit = 50
+    const page = parseInt(req.query.page)
+    console.log('Requested Page:', page);
+    const offset = (page - 1) * 50
 
     try {
-        const response = await axios.get(apiUrl)
+        const response = await axios.get(apiUrl, {
+            params: {
+                language: 2,
+                limit: limit,
+                offset: offset
+            }
+        })
         const exercises = response.data.results
         const exerciseNames = exercises.map(exercise => exercise.name)
 
-        res.status(200).render('12_newworkout', {exerciseNames})
+        const totalItems = response.data.count;
+        const totalPages = Math.ceil(totalItems / limit);
+
+        res.status(200).render('12_newworkout', {
+            exerciseNames,
+            currentPage: page,
+            totalPages: totalPages
+        });
     } catch (error) {
         console.error('Error fetching exercises data:', error)
         res.status(500).json({ error: 'Failed to fetch exercises data' });
