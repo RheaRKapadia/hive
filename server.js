9dbcc5e4453140f7e34faf0b6b55bedc719810d1
Lane20247 <lavne123@gmail.com>
2024-08-25 23:54:59 -0500

    commenting out firebase stuff for the meantime

--- server.js
+++ server.js
@@ -5,25 +5,25 @@ const serviceAccount = require('./serviceAccountKey.json')
 const path = require('path');
 
 
-// Initialize Firebase Admin SDK using environment variables
-admin.initializeApp({
-    credential: admin.credential.cert({
-      "type": process.env.FIREBASE_TYPE,
-      "project_id": process.env.FIREBASE_PROJECT_ID,
-      "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-      "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-      "client_id": process.env.FIREBASE_CLIENT_ID,
-      "auth_uri": process.env.FIREBASE_AUTH_URI,
-      "token_uri": process.env.FIREBASE_TOKEN_URI,
-      "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-      "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
-    }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL
-  });
+// // Initialize Firebase Admin SDK using environment variables
+// admin.initializeApp({
+//     credential: admin.credential.cert({
+//       "type": process.env.FIREBASE_TYPE,
+//       "project_id": process.env.FIREBASE_PROJECT_ID,
+//       "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+//       "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+//       "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+//       "client_id": process.env.FIREBASE_CLIENT_ID,
+//       "auth_uri": process.env.FIREBASE_AUTH_URI,
+//       "token_uri": process.env.FIREBASE_TOKEN_URI,
+//       "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+//       "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+//     }),
+//     databaseURL: process.env.FIREBASE_DATABASE_URL
+//   });
 
-  // Initialize Firestore
-  const db = admin.firestore();
+//   // Initialize Firestore
+//   const db = admin.firestore();
 
 //middleware
 app.use(logger)
